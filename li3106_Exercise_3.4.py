4e394541a5f311a96c1a57c5f456e85549718ac8
Pin-Ching Li <li3106@scholar-fe05.rcac.purdue.edu>
2020-05-07 20:12:07 -0400

    regrad_request_thanks

--- li3106_Exercise_3.4.py
+++ li3106_Exercise_3.4.py
@@ -20,14 +20,4 @@ def print_twice(St):
     for times in range(2):
         print(St)
 # do the function print_twice twice with the argument: spam string
-=======
-#define a function with two arguments: a function and a value
-def do_twice(fun, val):
-    for times in range(2):
-        fun(val)
-#define a function with a value for print twice
-def print_twice(St):
-    for times in range(2):
-        print(St)
->>>>>>> 70899d371d4aee8548849a4f7f9dc9444e0bc68f
 do_twice(print_twice, 'spam')
